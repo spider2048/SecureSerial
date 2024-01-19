3f090a39b75cf03a0a4f55e72b6176d834acf8ec
Sp1d3R <aspy1337@gmail.com>
2024-01-19 23:10:08 +0530

    Add README and Report

--- Main.ino
+++ Main.ino
@@ -1,10 +1,6 @@
 #include "secure_serial.h"
 secure_serial bt;
 
-// const uint8_t shared_secret[] = {
-//     116,246,174,229,44,159,237,86,47,14,85,149,142,79,114,211,165,217,188,145,219,201,54,164,31,238,147,204,51,117,251,46
-// };
-
 void setup() {
     Serial.begin(115200);
     LOG_START
---
+++ README.md
@@ -0,0 +1,9 @@
+# Secure Serial
+
+> Sp1d3R | 2023
+
+## Description
+
+A secure serial implementation for Arduino.
+
+For more information, refer to `report.pdf`.
